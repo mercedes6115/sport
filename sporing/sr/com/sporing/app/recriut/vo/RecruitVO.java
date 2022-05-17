bd456b35c39935f447b599546f96ca76c7421e59
mercedes6115 <bp6115@naver.com>
2022-05-17 22:44:34 +0900

    plz

---
+++ sporing/sr/com/sporing/app/recriut/vo/RecruitVO.java
@@ -0,0 +1,100 @@
+package com.sporing.app.recriut.vo;
+
+public class RecruitVO {
+	private String m_recriut_category;
+	private String m_recriut_grade;
+	private String m_recriut_gender;
+	private String m_recriut_ground_name;
+	private String m_recriut_ground_addr;
+	private String m_recriut_game_date;
+	private String m_recriut_start_time;
+	private String m_recriut_end_time;
+	private String m_recriut_fee;
+	private String m_recriut_content;
+	private String m_recriut_re_user;
+	public String getM_recriut_category() {
+		return m_recriut_category;
+	}
+	public void setM_recriut_category(String m_recriut_category) {
+		this.m_recriut_category = m_recriut_category;
+	}
+	public String getM_recriut_grade() {
+		return m_recriut_grade;
+	}
+	public void setM_recriut_grade(String m_recriut_grade) {
+		this.m_recriut_grade = m_recriut_grade;
+	}
+	public String getM_recriut_gender() {
+		return m_recriut_gender;
+	}
+	public void setM_recriut_gender(String m_recriut_gender) {
+		this.m_recriut_gender = m_recriut_gender;
+	}
+	public String getM_recriut_ground_name() {
+		return m_recriut_ground_name;
+	}
+	public void setM_recriut_ground_name(String m_recriut_ground_name) {
+		this.m_recriut_ground_name = m_recriut_ground_name;
+	}
+	public String getM_recriut_ground_addr() {
+		return m_recriut_ground_addr;
+	}
+	public void setM_recriut_ground_addr(String m_recriut_ground_addr) {
+		this.m_recriut_ground_addr = m_recriut_ground_addr;
+	}
+	public String getM_recriut_game_date() {
+		return m_recriut_game_date;
+	}
+	public void setM_recriut_game_date(String m_recriut_game_date) {
+		this.m_recriut_game_date = m_recriut_game_date;
+	}
+	public String getM_recriut_start_time() {
+		return m_recriut_start_time;
+	}
+	public void setM_recriut_start_time(String m_recriut_start_time) {
+		this.m_recriut_start_time = m_recriut_start_time;
+	}
+	public String getM_recriut_end_time() {
+		return m_recriut_end_time;
+	}
+	public void setM_recriut_end_time(String m_recriut_end_time) {
+		this.m_recriut_end_time = m_recriut_end_time;
+	}
+	public String getM_recriut_fee() {
+		return m_recriut_fee;
+	}
+	public void setM_recriut_fee(String m_recriut_fee) {
+		this.m_recriut_fee = m_recriut_fee;
+	}
+	public String getM_recriut_content() {
+		return m_recriut_content;
+	}
+	public void setM_recriut_content(String m_recriut_content) {
+		this.m_recriut_content = m_recriut_content;
+	}
+	public String getM_recriut_re_user() {
+		return m_recriut_re_user;
+	}
+	public void setM_recriut_re_user(String m_recriut_re_user) {
+		this.m_recriut_re_user = m_recriut_re_user;
+	}
+	public RecruitVO(String m_recriut_category, String m_recriut_grade, String m_recriut_gender,
+			String m_recriut_ground_name, String m_recriut_ground_addr, String m_recriut_game_date,
+			String m_recriut_start_time, String m_recriut_end_time, String m_recriut_fee, String m_recriut_content,
+			String m_recriut_re_user) {
+		super();
+		this.m_recriut_category = m_recriut_category;
+		this.m_recriut_grade = m_recriut_grade;
+		this.m_recriut_gender = m_recriut_gender;
+		this.m_recriut_ground_name = m_recriut_ground_name;
+		this.m_recriut_ground_addr = m_recriut_ground_addr;
+		this.m_recriut_game_date = m_recriut_game_date;
+		this.m_recriut_start_time = m_recriut_start_time;
+		this.m_recriut_end_time = m_recriut_end_time;
+		this.m_recriut_fee = m_recriut_fee;
+		this.m_recriut_content = m_recriut_content;
+		this.m_recriut_re_user = m_recriut_re_user;
+	}
+	public RecruitVO() {;}
+	
+}
